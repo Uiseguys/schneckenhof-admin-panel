61ce804beba106df51bbfa5bfdd4458b4155909e
ahadortiz <ahadortiz@outlook.com>
2018-03-27 20:14:24 -0300

    fix router issue

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/dist"));
+router.get('/*', (req, res) => {
+  const contents = fs.readFileSync(
+    path.resolve(__dirname, './dist', 'index.html'),
+    'utf8'
+  );
+  res.send(contents);
+});
+
 app.listen(process.env.PORT || 8080);
