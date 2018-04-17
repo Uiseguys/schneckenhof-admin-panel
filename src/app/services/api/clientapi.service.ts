a2f4c716eff121f004507b7ab33f6b9e8477303e
ahadortiz <ahadortiz@outlook.com>
2018-04-17 14:50:53 -0300

    add pdf download menu

--- src/app/services/api/clientapi.service.ts
+++ src/app/services/api/clientapi.service.ts
@@ -5,33 +5,32 @@ import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
-import {Api} from "./api.service";
-import {Observable, Subject} from "rxjs";
+import { Api } from './api.service';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable()
 export class ClientApiService extends Api {
+  private transactionObserver = new Subject();
+  private transactionRefresher;
 
-    private transactionObserver = new Subject();
-    private transactionRefresher;
+  // ---------- auth api ----------
+  login(info) {
+    return this.post('/CustomUsers/login', info);
+  }
 
-    // ---------- auth api ----------
-    login(info) {
-        return this.post('/CustomUsers/login', info);
-    }
+  register(info) {
+    return this.post('/CustomUsers', info);
+  }
 
-    register(info) {
-        return this.post('/CustomUsers', info);
-    }
+  logout() {
+    return this.post('/CustomUsers/logout', {});
+  }
 
-    logout() {
-        return this.post('/CustomUsers/logout', {});
-    }
+  getUserInfo(id) {
+    const filter = {
+      include: ['roles']
+    };
 
-    getUserInfo(id) {
-        const filter = {
-            include: ['roles'],
-        }
-
-        return this.get(`/CustomUsers/${id}?filter=${JSON.stringify(filter)}`);
-    }
+    return this.get(`/CustomUsers/${id}?filter=${JSON.stringify(filter)}`);
+  }
 }
