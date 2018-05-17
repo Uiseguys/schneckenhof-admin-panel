22cd049220faede951e6825696b85530ef980e30
ahadortiz <ahadortiz@outlook.com>
2018-05-17 09:14:02 -0300

    merge develop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  siteUrl: "",
-  apiUrl: "https://schneckenhof-api.herokuapp.com/api"
-=======
   siteUrl: '',
-  apiUrl: 'https://dev-schneckenhof-api.herokuapp.com/api',
+  apiUrl: 'https://schneckenhof-api.herokuapp.com/api',
   pdfUrl: 'https://www.pdf-aas.io'
->>>>>>> develop
 };
