6c34c20d585b3a90d655a3badcabdd70457f5f8b
Bablu Kumar <bablu@rudrainnovatives.com>
2018-11-28 18:17:53 +0530

    made build with changes

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
-app.get('/*', (req, res) => {
+console.log("testing ..************ .. outside");
+app.get('*', (req, res) => {
   const contents = fs.readFileSync(
     path.resolve(__dirname, './dist', 'index.html'),
     'utf8'
   );
+  console.log("**************************")
+
   res.send(contents);
 });
 
