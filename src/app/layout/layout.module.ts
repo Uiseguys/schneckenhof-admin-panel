8b32349160b5f5e3b2ba9ce086d4658ac2f1e60c
Cris Shaki <cmshaki@gmail.com>
2020-11-09 20:07:01 +0300

    Removed all sub sections except for news, and removed all settings except e-mail

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { SharedModule } from '../shared/shared.module';
-import { SettingService } from '../pages/setting/setting.service';
-import { DashboardLayoutComponent } from './dashboardlayout/dashboardlayout.component';
-import { GoTrueJs } from '../services/netlify/gotrue-js.service';
+import { SharedModule } from "../shared/shared.module";
+import { SettingService } from "../pages/setting/setting.service";
+import { DashboardLayoutComponent } from "./dashboardlayout/dashboardlayout.component";
+import { GoTrueJs } from "../services/netlify/gotrue-js.service";
 
 @NgModule({
-    imports: [CommonModule, RouterModule, BrowserAnimationsModule, SharedModule],
-    providers: [SettingService, GoTrueJs],
-    declarations: [DashboardLayoutComponent],
-    exports: [DashboardLayoutComponent, SharedModule]
+  imports: [CommonModule, RouterModule, BrowserAnimationsModule, SharedModule],
+  providers: [SettingService, GoTrueJs],
+  declarations: [DashboardLayoutComponent],
+  exports: [DashboardLayoutComponent, SharedModule]
 })
 export class LayoutModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,54 +1,58 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { SharedModule } from '../shared/shared.module';
-import { AuthGuardResolve } from '../services/authguard/authguard.service';
-import { ServicesModule } from '../services/services.module';
-import { LayoutModule } from '../layout/layout.module';
-import { DashboardLayoutComponent } from '../layout/dashboardlayout/dashboardlayout.component';
-import { LoginPage } from './login/login.page';
-import { RegisterPage } from './register/register.page';
+import { SharedModule } from "../shared/shared.module";
+import { AuthGuardResolve } from "../services/authguard/authguard.service";
+import { ServicesModule } from "../services/services.module";
+import { LayoutModule } from "../layout/layout.module";
+import { DashboardLayoutComponent } from "../layout/dashboardlayout/dashboardlayout.component";
+import { LoginPage } from "./login/login.page";
+import { RegisterPage } from "./register/register.page";
 
 export const routes = [
   {
-    path: 'login',
+    path: "login",
     component: LoginPage,
     canActivate: [AuthGuardResolve]
   },
   {
-    path: 'dashboard',
+    path: "dashboard",
     component: DashboardLayoutComponent,
     resolve: {
       user: AuthGuardResolve
     },
     children: [
-      { path: '', redirectTo: '/dashboard/red', pathMatch: 'full' },
+      //{ path: '', redirectTo: '/dashboard/red', pathMatch: 'full' },
 
+      //{
+      //path: 'packaging',
+      //loadChildren: () => import('./packaging/packaging.module').then(m => m.PackagingModule)
+      //},
+      //{ path: 'payments', loadChildren: () => import('./order/order.module').then(m => m.OrderModule) },
+      //{
+      //path: 'templates',
+      //loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)
+      //},
+      //{
+      //path: 'images',
+      //loadChildren: () => import('./image/image.module').then(m => m.ImageModule)
+      //},
       {
-        path: 'packaging',
-        loadChildren: () => import('./packaging/packaging.module').then(m => m.PackagingModule)
-      },
-      { path: 'payments', loadChildren: () => import('./order/order.module').then(m => m.OrderModule) },
-      {
-        path: 'templates',
-        loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)
+        path: "settings",
+        loadChildren: () =>
+          import("./setting/setting.module").then(m => m.SettingModule)
       },
       {
-        path: 'images',
-        loadChildren: () => import('./image/image.module').then(m => m.ImageModule)
-      },
-      {
-        path: 'settings',
-        loadChildren: () => import('./setting/setting.module').then(m => m.SettingModule)
-      },
-      { 
-        path: 'news',
-        loadChildren: () => import('./news/news.module').then(m => m.NewsModule)
-     },
-      { path: ':type', loadChildren: () => import('./wine/wine.module').then(m => m.WineModule) }
+        path: "", // Original path is news
+        loadChildren: () => import("./news/news.module").then(m => m.NewsModule)
+      }
+      //{
+      //path: ":type",
+      //loadChildren: () => import("./wine/wine.module").then(m => m.WineModule)
+      //}
     ]
   },
-  { path: '**', redirectTo: 'login' }
+  { path: "**", redirectTo: "login" }
   // Not found
 ];
 
